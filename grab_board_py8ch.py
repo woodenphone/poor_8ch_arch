315aeb99910c2cbbfe2e17fb44a2502599aa69dd
James <j1459.j1459@gmail.com>
2018-11-18 22:30:14 +0800

    Started readme. Coded more thread insert logic. Added more DB columns.

--- grab_board_py8ch.py
+++ grab_board_py8ch.py
@@ -35,15 +35,13 @@ Base = declarative_base()# Setup system to keep track of tables and classes
 
 
 
-def table_factory_board(board_name):# Boards table
+def table_factory_boards():# Boards table
     """ We're Java now!
     Make the 'x_board' table for an 8chan board.
     see https://stackoverflow.com/questions/19163911/dynamically-setting-tablename-for-sharding-in-sqlalchemy
     TODO: Sane database design
     """
-    logging.debug('table_factory_board() args={0!r}'.format(locals()))# Record arguments.
-    assert(type(board_name) in [str, unicode])
-    table_name = '{0}_board'.format(board_name)
+    table_name = 'boards'
     logging.debug('Naming the board table {0!r}'.format(table_name))
     class Board(Base):
         __tablename__ = table_name
@@ -109,6 +107,7 @@ def table_factory_posts(board_name):# Posts table
         __tablename__ = table_name
         # From: https://github.com/bibanon/py8chan/blob/master/py8chan/post.py#L13
         # Use 'p_VarName' format to maybe avoid fuckery. (Name confusion, name collission, reserved keywords such as 'id', etc)
+        p_thread_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
         # py8chan.Post class attributes
         p_post_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
         p_poster_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
@@ -169,12 +168,29 @@ def table_factory_files(board_name):# File table
         m_thumbnail_fname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
         m_thumbnail_url = sqlalchemy.Column(sqlalchemy.String, nullable=True)
         # 'Unused' attributes:
+        # archive-side values
+        archive_filename = sqlalchemy.Column(sqlalchemy.String, nullable=True)# What the file is called on the archive's disk
+        file_saved = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)# Set to False if the file is lost from the server. Will be set to True when file is saved.
+        forbidden = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)# If True any file matching this MD5 will not be downloaded.
         # Misc recordkeeping: (internal use and also for exporting dumps more easily)
         row_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True, default=datetime.datetime.utcnow)
         row_updated = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True, onupdate=datetime.datetime.utcnow)
         primary_key = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     return File
 
+
+
+##class Luggage():
+##    """For carrying things around between functions because I don't know what I'm doing.
+##    TODO: Replace this with something that doesn't set off all my shit-code alarms.
+##    """
+##    def __init__(self, Boards, Threads, Posts, File):
+##        # Hold onto DB table classes
+##        Boards = Boards
+##        Threads = Threads
+##        Posts = Posts
+##        File = File# TODO: Maybe rename this class to avoid conflicts with builtin 'file'
+##        #
 
 
 def convert_filepath_to_connect_string(filepath):
@@ -185,12 +201,30 @@ def convert_filepath_to_connect_string(filepath):
     return connect_string
 
 
+def get_table_from_base(board_name, table_type):
+    """Get the a reference to a table.
+    Uses sqlalchemy Base."""
+    return Base.metadata.tables['{0}_{1}'.format(board_name, table_type)]
 
-def insert_thread(ses, board, thread_id):
+
+def insert_thread(ses, board_name, board, thread_id):
     """Fetch and insert one thread.
     ses: Sqlalchemy DB session
     board: py8chan Board instance
+    WARNING: May overwrite existing post/thread data in table!
+    TODO: Look into resolutions for this potential issue.
     """
+    logging.debug('Fetching thread: {0}'.format(thread_id))
+    Boards = get_table_from_base(board_name=board_name, table_type='boards')
+    Threads = get_table_from_base(board_name=board_name, table_type='threads')
+    Posts = get_table_from_base(board_name=board_name, table_type='posts')
+    Files = get_table_from_base(board_name=board_name, table_type='files')
+
+    # Poll DB for existing posts in this thread
+    before_add_posts_q = ses.query(Posts)\
+        .filter(p_thread_id=thread_id)
+
+    # Load thread from site
     thread = board.get_thread(thread_id)
 
     # Shove API data into the DB
@@ -198,10 +232,30 @@ def insert_thread(ses, board, thread_id):
     logging.info('Fake thread insert')
     logging.info('thread={0}'.format(thread))
     # TODO: Insert thread-level data into DB
+    new_thread = Threads(
+        t_thread_id = thread.id,
+        t_board = 0,# TODO Foreign key (thread-to-board association)
+        t_last_reply_id = thread.last_reply_id,
+        t_closed = thread.closed,
+        t_sticky = thread.sticky,
+        t_topic = thread.topic,
+        t_posts = None,# TODO FIXME (post-to-thread association)
+        t_all_posts = None,# TODO FIXME (post-to-thread association)
+        t_url = thread.url,
+    )
+    ses.add(new_thread)
+    logging.debug('Staged thread entry')
+
     for current_post in thread.all_posts:
         # Put post-level data into DB
-        post = Post()
         logging.info('current_post={0}'.format(current_post))
+        new_post = Posts(
+            p_thread_id = thread_id,
+        )
+        current_post.post_id
+        ses.add(new_post)
+        logging.debug('Staged post entry')
+
         # TODO: Insert post to DB
         if current_post.has_file:
             logging.info('Post has file(s)')
@@ -209,7 +263,66 @@ def insert_thread(ses, board, thread_id):
                 # Put file-level data into DB
                 logging.info('Fake file insert')
                 logging.info('current_file={0}'.format(current_file))
+
                 # TODO Fetch media absent from DB
+                # Check if file has entry in DB (look for md5 and file_saved=True)
+                # If any rows returned, skip download
+
+                # TODO: Skip forbidden images
+                # Check if file has entry in DB (look for md5 and forbidden=True)
+                # If any rows returned, skip download
+
+
+                img_check_q = ses.query(Images)\
+                    .filter(m_file_md5=current_file.file_md5)
+                img_check_result = img_check_q.first()
+                if img_check_result:
+                    # Already saved?
+                    if (img_check_result.file_saved == True):# TODO
+                        logging.debug('File already saved: {0}'.format(current_file))
+                        continue
+                    # Forbidden?
+                    elif (img_check_result.forbidden == True):# TODO
+                        logging.debug('File is forbidden: {0}'.format(current_file))
+                        logging.debug('File is forbidden: {0}'.format(current_file))
+                        continue
+                    else:
+                        logging.debug('Downloading file: {0}'.format(current_file))
+
+                        # Load remote file
+                        # TODO
+
+                        # Save file to disk
+                        # TODO
+
+                        # Insert file row
+                        new_file = File(
+                        # py8chan columns
+                            m_file_md5 = current_file.file_md5,
+                            m_file_md5_hex = current_file.file_md5_hex,
+                            m_filename_original = current_file.filename_original,
+                            m_filename = current_file.filename,
+                            m_file_url = current_file.file_url,
+                            m_file_extension = current_file.file_extension,
+                            m_file_size = current_file.file_size,
+                            m_file_width = current_file.file_width,
+                            m_file_height = current_file.file_height,
+                            m_thumbnail_width = current_file.thumbnail_width,
+                            m_thumbnail_height = current_file.thumbnail_height,
+                            m_thumbnail_fname = current_file.thumbnail_fname,
+                            m_thumbnail_url = current_file.thumbnail_url,
+                            # archive-side data (File present on disk? File forbidden?)
+                            file_saved = True,
+                            forbidden = True
+                        )
+                        ses.add(new_file)
+                        logging.debug('Staged file entry')
+                continue
+            logging.debug('Finished this post\'s files')
+            continue
+        logging.debug('Finished this thread\'s posts')
+        continue
+    logging.info('Fetched thread')
     return
 
 
@@ -220,10 +333,16 @@ def dev():
     connection_string = convert_filepath_to_connect_string(filepath=db_filepath)
     thread_id = 316521 # https://8ch.net/pone/res/316521.html
     # Prepare board DB classes/table mappers
-    Board = table_factory_board(board_name)
+    Boards = table_factory_boards()
     Threads = table_factory_threads(board_name)
     Posts = table_factory_posts(board_name)
-    File = table_factory_files(board_name)
+    Files = table_factory_files(board_name)
+##    lug = Luggage(# TODO FIX THIS TERRIBLE FEELING MONSTROSITY
+##        Boards=Boards,
+##        Threads=Threads,
+##        Posts=Posts,
+##        Files=Files
+##    )
 
     # Setup/start/connect to DB
     logging.debug('Connecting to DB')
@@ -251,24 +370,18 @@ def dev():
     # Shove API data into the DB
     insert_thread(
         ses=session,
+        board_name=board_name,
         board=pone,
         thread_id=thread_id
     )
 
+    # Persist data now that thread has been grabbed
+    session.commit()
 
-##    # Put thread-level data into DB
-##    logging.info('Fake thread insert')
-##    logging.info('thread={0}'.format(thread))
-##    for current_post in thread.all_posts:
-##        # Put post-level data into DB
-##        logging.info('Fake post insert')
-##        logging.info('current_post={0}'.format(current_post))
-##        if current_post.has_file:
-##            logging.info('Post has file(s)')
-##            for current_file in current_post.all_files():
-##                # Put file-level data into DB
-##                logging.info('Fake file insert')
-##                logging.info('current_file={0}'.format(current_file))
+    logging.info('Ending DB session')
+    session.close()# Release connection back to pool.
+    engine.dispose()# Close all connections.
+
     logging.warning('exiting dev()')
     return
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+poor_8ch_arch Readme
+This is not ready for use.
+TODO: Markdown formatting
+TODO: Write readme
